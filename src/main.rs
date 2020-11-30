e689ab2faad4647968cac03cca479d2ad2cb5ceb
Erik Hollensbe <github@hollensbe.org>
2020-11-29 20:56:44 -0800

    use tokio
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ mod template;
 
 #[macro_use]
 extern crate clap;
+use tokio;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let c = commands::Commands {};
     if let Err(e) = c.evaluate() {
         println!("error: {}", e.to_string());
