5958e4a338cecd81888639e80c164a1db0a91ca2
Erik Hollensbe <git@hollensbe.org>
2024-01-19 01:33:15 -0800

    flesh out launcher module tree on the filesystem a bit
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

---
+++ src/launcher/emulators/qemu/mod.rs
@@ -0,0 +1,30 @@
+pub mod linux;
+
+use crate::{
+    launcher,
+    qmp::{Client, UnixSocket},
+    storage::{DirectoryStorageHandler, StorageHandler},
+};
+use anyhow::Result;
+use std::os::unix::net::UnixStream;
+
+pub struct EmulatorController {
+    dsh: DirectoryStorageHandler,
+}
+
+impl EmulatorController {
+    pub fn new(dsh: DirectoryStorageHandler) -> Self {
+        Self { dsh }
+    }
+}
+
+impl launcher::EmulatorController for EmulatorController {
+    fn shutdown(&self, name: &str) -> Result<()> {
+        let stream = UnixStream::connect(self.dsh.monitor_path(name)?)?;
+        let mut us = UnixSocket::new(stream)?;
+        us.handshake()?;
+        us.send_command("qmp_capabilities", None)?;
+        us.send_command("system_powerdown", None)?;
+        Ok(())
+    }
+}
---
+++ src/launcher/mod.rs
@@ -0,0 +1,61 @@
+pub mod emulators;
+
+use crate::storage::DirectoryStorageHandler;
+use anyhow::{anyhow, Result};
+use fork::{daemon, Fork};
+use std::{path::PathBuf, process::Command};
+
+pub struct RuntimeConfig {
+    pub cdrom: Option<PathBuf>,
+    pub extra_disk: Option<PathBuf>,
+    pub headless: bool,
+    pub dsh: DirectoryStorageHandler,
+}
+
+pub trait Emulator {
+    fn args(&self, vm_name: &str, rc: &RuntimeConfig) -> Result<Vec<String>>;
+    fn bin(&self) -> Result<String>;
+}
+
+pub trait EmulatorController {
+    fn shutdown(&self, vm_name: &str) -> Result<()>;
+}
+
+pub struct Launcher {
+    emu: Box<dyn Emulator>,
+    rc: RuntimeConfig,
+}
+
+impl Launcher {
+    pub fn new(emu: Box<dyn Emulator>, rc: RuntimeConfig) -> Self {
+        Self { emu, rc }
+    }
+
+    pub fn launch(&self, vm_name: &str, detach: bool) -> Result<Option<std::process::ExitStatus>> {
+        let args = self.emu.args(vm_name, &self.rc)?;
+        let mut cmd = Command::new(self.emu.bin()?);
+        let spawnres = if detach {
+            if let Ok(Fork::Child) = daemon(false, false) {
+                cmd.args(args).spawn()
+            } else {
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "could not fork",
+                ))
+            }
+        } else {
+            cmd.args(args).spawn()
+        };
+
+        match spawnres {
+            Ok(mut child) => {
+                if !detach {
+                    Ok(Some(child.wait()?))
+                } else {
+                    Ok(None)
+                }
+            }
+            Err(e) => Err(anyhow!(e)),
+        }
+    }
+}
