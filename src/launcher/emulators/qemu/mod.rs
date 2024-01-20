17f788b07c4c6944a19f04dc9132c2ea27bfa02f
Erik Hollensbe <git@hollensbe.org>
2024-01-19 21:44:40 -0800

    better error message for shutting down a dead vm
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/launcher/emulators/qemu/mod.rs
+++ src/launcher/emulators/qemu/mod.rs
@@ -5,7 +5,7 @@ use crate::{
     qmp::{Client, UnixSocket},
     storage::{DirectoryStorageHandler, StorageHandler},
 };
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::os::unix::net::UnixStream;
 
 pub struct EmulatorController {
@@ -21,11 +21,15 @@ impl EmulatorController {
 impl launcher::EmulatorController for EmulatorController {
     fn shutdown(&self, name: &str) -> Result<()> {
         let _ = std::fs::remove_file(self.dsh.vm_root(name)?.join("pid"));
-        let stream = UnixStream::connect(self.dsh.monitor_path(name)?)?;
-        let mut us = UnixSocket::new(stream)?;
-        us.handshake()?;
-        us.send_command("qmp_capabilities", None)?;
-        us.send_command("system_powerdown", None)?;
-        Ok(())
+        match UnixStream::connect(self.dsh.monitor_path(name)?) {
+            Ok(stream) => {
+                let mut us = UnixSocket::new(stream)?;
+                us.handshake()?;
+                us.send_command("qmp_capabilities", None)?;
+                us.send_command("system_powerdown", None)?;
+                Ok(())
+            }
+            Err(_) => Err(anyhow!("{} is not running or not monitored", name)),
+        }
     }
 }
