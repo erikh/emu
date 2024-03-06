12435a9972feb5164e4a5020ecde90ef6c7611e8
Erik Hollensbe <git@hollensbe.org>
2024-03-06 09:07:26 -0800

    tests for address serialization
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/network/address.rs
+++ src/network/address.rs
@@ -53,6 +53,11 @@ impl Visitor<'_> for AddressVisitor {
 
         let ip: IpAddr = parts[0].parse().map_err(|e| E::custom(e))?;
         let mask: u8 = parts[1].parse().map_err(|e| E::custom(e))?;
+
+        if (ip.is_ipv4() && mask > 32) || (ip.is_ipv6() && mask > 128) {
+            return Err(E::custom("Mask is larger than possible for this IP class"));
+        }
+
         Ok(Address { ip, mask })
     }
 }
@@ -63,5 +68,52 @@ impl<'de> Deserialize<'de> for Address {
         D: serde::Deserializer<'de>,
     {
         deserializer.deserialize_str(AddressVisitor)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_serde() -> Result<()> {
+        let a: Vec<Address> = serde_json::from_str(r#"["192.168.1.1/32"]"#)?;
+        assert_eq!(
+            a[0],
+            Address {
+                ip: IpAddr::from_str("192.168.1.1")?,
+                mask: 32
+            }
+        );
+
+        let a: Vec<Address> = serde_json::from_str(r#"["fe80::/128"]"#)?;
+        assert_eq!(
+            a[0],
+            Address {
+                ip: IpAddr::from_str("fe80::")?,
+                mask: 128
+            }
+        );
+
+        let errors = vec![
+            "192.168.1.1",
+            "192.168.1.1/64",
+            "fe80::/129",
+            "fe80::",
+            "hijk::",
+        ];
+
+        for error in errors {
+            assert!(serde_json::from_str::<Vec<Address>>(&format!(r#"["{}"]"#, error)).is_err());
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_hash() -> Result<()> {
+        Ok(())
     }
 }
